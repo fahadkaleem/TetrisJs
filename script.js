587d0d55f57f79d0df44ba1d6be909236e75ee4b
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-08-17 09:49:29 -0700

    Added lot of code

--- script.js
+++ script.js
@@ -13,10 +13,50 @@ const matrix = [
     [0, 1, 0],
 ];
 
+function createMatrix(w,h){
+    const matrix = [];
+    while (h--){
+    matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
+function collide(arena,player){
+    const [m,o] = [player.matrix,player.pos];
+    for(let y=0;y<matrix.length;++y){
+        for(let x=0;x<m[y].length;++x){
+              if(m[y][x]!==0 &&
+                (arena[y+o.y] &&
+                 arena[y+o.y][x+o.x])!==0){
+                return true
+            }
+        }
+    }
+    return false;
+}
+
 function draw() {
     context.fillStyle = 'black';
     context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(arena,{x:0,y:0});
     drawMatrix(player.matrix, player.pos);
+}
+
+function playerDrop(){
+    player.pos.y++;
+    if(collide(arena,player)){
+        player.pos.y--;
+        merge(arena,player);
+        player.pos.y=0;
+    }
+    dropCounter=0;
+}
+
+function playerMove(direction) {
+    player.pos.x+=direction;
+    if(collide(arena,player)){
+        player.pos.x -= direction;
+    }
 }
 
 let dropCounter = 0;
@@ -48,6 +88,18 @@ function drawMatrix(matrix, offset) {
     });
 }
 
+function merge(arena,player) {
+player.matrix.forEach((row,y) =>{
+    row.forEach((value,x)=>{
+    if(value!==0){
+        arena[y+player.pos.y][x+player.pos.x] = value;
+    }
+    });
+    });
+}
+
+const arena = createMatrix(12,20);
+
 const player = {
     pos: {x: 5, y: 5},
     matrix: matrix,
@@ -55,7 +107,13 @@ const player = {
 
 document.addEventListener('keydown',event =>{
    if(event.keyCode===37){
-       player.pos.x--;
+       playerMove(-1)
+   }
+   else if(event.keyCode===39){
+        playerMove(1);
+   }
+   else if(event.keyCode===40){
+playerDrop();
    }
 
 });
